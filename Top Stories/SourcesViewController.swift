412a1e1c6a7d699de020875baaf54ce50126dc75
Andrew James Kinsey <akinsey@andrews-macbook-air-123.local>
2017-09-07 12:27:58 -0500

    Parse JSON data

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -19,11 +19,39 @@ class SourceViewController: UITableViewController
         super.viewDidLoad()
         self.title = "News Source"
         let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
-        
+        if let url = URL(string: query)
+        {
+            if let data = try? Data(contentsOf: url) //optional unwrapping "if it's there"
+            {
+                let json = try! JSON(data: data)     //forced unwrapping
+                if json["status"] == "ok"
+                {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError() //if anything fails
     }
-
     
-
-
+    func parse(json: JSON)
+    {
+        for result in json["sources"].arrayValue
+        {
+            let id = result["id"].stringValue
+            let name = result["name"].stringValue
+            let description = result["description"].stringValue
+            let source = ["id": id, "name": name, "description": description]
+            sources.append(source)
+        }
+        tableView.reloadData()
+    }
+    
+    func loadError()
+    {
+        let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the news feed.", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
 }
 
